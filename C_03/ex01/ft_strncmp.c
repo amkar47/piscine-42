d82e74a59165c03fdb86b74ade965800c55971fe
Noa Amath <namath@z2r10p6.42lyon.fr>
2022-07-27 14:15:19 +0200

    bon c'est chiant

--- C_03/ex01/ft_strncmp.c
+++ C_03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: namath <namath@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 08:55:01 by namath            #+#    #+#             */
-/*   Updated: 2022/07/27 12:02:05 by namath           ###   ########lyon.fr   */
+/*   Updated: 2022/07/27 14:14:55 by namath           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,4 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 	return (s1[i] - s2[i]);
-}
-
-#include <stdio.h>
-#include <string.h>
-int main ()
-{
-    char    s1[30] = "abcdeghiz";
-    char    s2[30] = "abcdeghia";
-    printf("%d", ft_strncmp(s1, s2, 3));
-    printf("\n%d", strncmp(s1, s2, 3));
-    return(0);
 }
